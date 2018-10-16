805215bfd1dc91ccb83ed3dafe95a69eccaf8cea
anallely753 <anallely.0710@gmail.com>
2018-10-16 01:38:02 -0500

    Practicando JavaScript

---
+++ Caracola magica/js/futuro.js
@@ -0,0 +1,20 @@
+while(true)
+{
+
+var Usuario = prompt("");
+var Computadora = Math.random();
+if (Computadora < 0.16) {
+    alert("Claro que si");
+} else if(Computadora <= 0.32) {
+    alert("Seamos positivos");
+} else if(Computadora <=.48){
+    alert("No lo creo");
+}else if(Computadora <=.64){
+    alert("Solo si te portas bien");
+}else if(Computadora <=.80){
+    alert("Algun dia");
+}else if(Computadora <=1){
+    alert("Obvio no");
+}
+}
+
